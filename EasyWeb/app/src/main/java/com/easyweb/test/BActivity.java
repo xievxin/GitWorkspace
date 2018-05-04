723bc24ce71bbb45c7aa2af5b19bda665c493b06
xiexin <xiex@chuangke18.com>
2018-05-04 11:57:26 +0800

    commit searchGTApps

--- EasyWeb/app/src/main/java/com/easyweb/test/BActivity.java
+++ EasyWeb/app/src/main/java/com/easyweb/test/BActivity.java
@@ -12,5 +12,6 @@ public class BActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
